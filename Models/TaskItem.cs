cd50fb9d4191507c4a45eb03b99ca012a77e7af0
Zishan Ansari <rs.azizansari@gmail.com>
2026-05-03 00:51:25 +0530

    feat: add Entity,Dbcontext

---
+++ Models/TaskItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.API.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(300)]
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public Priority Priority { get; set; }
+    }
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
